73c68091b850b0f40a26a1ec134ce6f29ee1dcdd
Drew Wheeler <wheelercs@protonmail.com>
2023-09-09 14:35:16 -0400

    Working on Robustness of System
    Working on making the robustness of the system better. Ensure server doesn't crap out if it loses connection and client doesn't exit when not supposed to.

--- Project 01 - Socket Programming/client.py
+++ Project 01 - Socket Programming/client.py
@@ -1,11 +1,14 @@
 import socket
 
+from math import ceil
+
 LOCALHOST = "127.0.0.1"
 HOST = LOCALHOST
 PORT = 21250
+PACKET_SIZE = 1024
 
 # Create a socket object
-server_socket = socket.socket()
+connection = socket.socket()
 
 # Define the server address and port
 server_address = HOST
@@ -13,7 +16,7 @@ server_port = PORT
 
 # Connect to the server
 try:
-    server_socket.connect((server_address, server_port))
+    connection.connect((server_address, server_port))
 except Exception as e:
     # Any interruption to connection process terminates program for safety
     print ("Unable to establish connection with server", server_address + ':' + str(server_port))
@@ -28,25 +31,37 @@ while True:
 
     # Send the message to the server
     try:
+        # Send header with message metadata
+        header_size = -1
         if message == "exit":
-            message_header = "dc"
+            header_size = 0
         else:
-            message_header = "m_size " + str (len (message))
-        server_socket.sendall (message_header.encode ("utf-8"))
-        server_socket.sendall (message.encode ("utf-8")) # Encode w/ UTF-8 to send message as binary
-    except Exception:
-        print ("Server at", server_address, "is currently unreachable")
+            header_size = len (message)
+        message_header = "m_size " + str (header_size)
+        connection.sendall (message_header.encode ("utf-8"))
+        
+        # Send message to server
+        if message != "exit":
+            s_message = ""
+            connection.sendall (message.encode ("utf-8")) # Encode w/ UTF-8 to send message as binary
+
+            # Get length of the message server is sending
+            s_header = connection.recv (PACKET_SIZE)
+            header_args = s_header.split (' ')
+            for arg in enumerate (header_args):
+                if (arg[1] == "m_size"):
+                    s_message_len = int (header_args[arg[0] + 1])
+            for packets in range (0, (ceil (s_message_len / PACKET_SIZE))):
+                s_packet = connection.recv (PACKET_SIZE)
+                s_message += s_packet.decode ("utf-8")
+            
+            print ("Server responded with: ")
+            print (s_message)
+
+    except Exception as e:
         break
 
-    # Receive and print the server's response
-    try:
-        server_message = (server_socket.recv (1024)).decode ("utf-8")
-    except Exception:
-        print ("Unable to receive data from server")
-        break
-    # BUG: When receiving large amounts of data, message is buffered causing queue to back up.
-    print (f"Response: {server_message}")
 
 # Close the client socket
-server_socket.shutdown(socket.SHUT_RDWR) # Prevent additional sending and receiving of data
-server_socket.close()
+connection.shutdown(socket.SHUT_RDWR) # Prevent additional sending and receiving of data
+connection.close()
--- Project 01 - Socket Programming/server.py
+++ Project 01 - Socket Programming/server.py
@@ -10,25 +10,23 @@ CONN_QUEUE_SIZE = 5
 PACKET_SIZE = 1024
 
 
+# Set up server socket
 # Create a socket object
 connection_socket = socket.socket()
-
 # Define the server address and port
 server_address = BROADCAST_ADDRESS
 server_port = PORT
-
 # Bind the socket to the server address
 connection_socket.bind ((server_address, server_port))
-
 # Listen for incoming connections (max 5 clients in the queue)
 connection_socket.listen (CONN_QUEUE_SIZE)
-
 print("< Server is listening on port", server_port, '>')
 
+
+# Run Server
 while True:
     # Wait for a client to connect
     connection, client_address = connection_socket.accept()
-    
     # Print a message to indicate the client connection
     print ("< Connection established with", client_address[0], '>')
 
@@ -36,35 +34,36 @@ while True:
     while True:
         # Receive data from the client
         try:
-            message: str = ""       # Message from client
-            message_len: int = 0    # Length of message from client
+            c_message: str = "" # Message from client
+            c_message_len: int = -1 # Length of message from client
             # Receive message header and parse for arguments
             packet_header = connection.recv (PACKET_SIZE).decode ("utf-8")
+            # Process and respond to the client's data
             packet_args = packet_header.split (' ')
             for arg in enumerate (packet_args):
                 if (arg[1] == "m_size"): # m_size: Length of message (in chars)
-                    message_len = int(packet_args[arg[0] + 1])
-                elif (arg[1] == "dc"): # dc: Client disconnect
-                    print ('<', client_address[0], "disconnected >")
-                    break
-            # Get packets and reconstruct message
-            # Use ceiling function to determine how many packets have been buffered
-            for packets in range (0, (ceil(message_len / PACKET_SIZE))):
-                packet = connection.recv (PACKET_SIZE)
-                message += packet.decode ("utf-8")
-            
-        except ConnectionResetError: # Client disconnected w/o notifying server
-            print (str(client_address[0]), "disconnected without warning")
-            break
-        except Exception: # Generic error when trying to read message
-            print ("Unable to receive data from", str(client_address[0]))
-            break
+                    c_message_len = int (packet_args[arg[0] + 1])
 
-        # Send the response back to the client
-        try:
-            connection.sendall (message.encode ("utf-8"))
-        except Exception:
-            print ("Unable to echo message back to", str(client_address[0]))
+            # Handles BOTH the client disconnecting normally and a dropped connection (such as client CTRL+C)
+            if (c_message_len <= 0):
+                print ("< Client", client_address[0], "disconnected >")
+                break
+            else:
+                # Get packets and reconstruct message
+                # Use ceiling function to determine how many packets have been buffered
+                for packets in range (0, (ceil (c_message_len / PACKET_SIZE))):
+                    packet = connection.recv (PACKET_SIZE)
+                    c_message += packet.decode ("utf-8")
+                
+                print (client_address[0], ':', client_address[1],"sent", c_message)
+
+                # Send the response back to the client
+                c_header = "m_size" + str (len (c_message))
+                connection.sendall (c_header)
+                connection.sendall (c_message.encode ("utf-8"))
+            
+        except Exception: # Generic error when trying to read message
+            print ("! Cannot communicate with", str(client_address[0]), '!')
             break
 
     # Close the client socket
