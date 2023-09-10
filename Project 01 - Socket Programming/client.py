10cc9adc77fab723873bb958febf20e93bea1e78
Drew Wheeler <wheelercs@protonmail.com>
2023-09-10 13:37:08 -0400

    Restarting... Again
    Having more issues. Starting from square 1.

--- Project 01 - Socket Programming/client.py
+++ Project 01 - Socket Programming/client.py
@@ -1,69 +1,22 @@
 import socket
 
-from math import ceil
+# Create a socket object
 
-# === Constants ====================================================================================
+# Define the server address and port
 
-LOCALHOST = "127.0.0.1"
-HOST = LOCALHOST
-PORT = 21250
-PACKET_SIZE = 2048
+# Connect to the server
+# Replace the following line with code to connect to the server
 
+while True:
+    # Get user input
+    message = input("Enter a message to send to the server (or 'exit' to quit): ")
+        
+    # Send the message to the server
+    # Replace the following line with code to send the message
 
-# === Function Definitions =========================================================================
+    # Receive and print the server's response
+    # Replace the following line with code to receive and print the response
+    # Make sure you are able to receive long messages
 
-def establish_conn (target_host: str = "127.0.0.1", host_port: int = 9999) -> socket.socket:
-    # Create a socket object
-    new_socket = socket.socket()
-    # Connect to the server
-    new_socket.connect ((target_host, host_port))
-    return new_socket
-
-def create_header (text: str) -> str:
-    if (text == "") or (text == "exit") or (text is None):
-        return "m_size 0"
-    else:
-        return ("m_size " + str (len (text)))
-
-def encode_text (text: str):
-    return (text.encode ("utf-8"))
-
-def decode_text (text) -> str:
-    return (text.decode ("utf-8"))
-    
-def forward_message (message: str, destination: socket.socket) -> bool:
-    header = create_header (message)
-    if (header == "m_size 0"):
-        return False
-    else:
-        try:
-            destination.sendall (encode_text (header))
-            destination.sendall (encode_text (message))
-        except Exception:
-            return False
-
-def get_echo (echo_source: socket.socket) -> str:
-    return "A"
-
-def echo_client (conn: socket.socket) -> None:
-    while True:
-        # Get user input
-        message = input("Enter a message to send to the server (or 'exit' to quit): ")
-        # Send the message to the server
-        forward_message (message, conn)
-        # Receive and print the server's response
-        new_message = get_echo (conn)
-        print ("Message Returned:", new_message)
-
-
-# === Main =========================================================================================
-
-if __name__ == "__main__":
-    s_connection = establish_conn (HOST, PORT)
-    if (s_connection is not None):
-        echo_client (s_connection)
-        # Close the client socket
-        s_connection.shutdown(socket.SHUT_RDWR) # Stop all incoming and outgoing transfers
-        s_connection.close()
-    else:
-        print ("Was unable to connect to", HOST, "on port", str (PORT))
+# Close the client socket
+# Replace the following line with code to close the client socket
--- Project 01 - Socket Programming/server.py
+++ Project 01 - Socket Programming/server.py
@@ -1,93 +1,38 @@
 import socket
 
-from math import ceil
-
-# === Constants ====================================================================================
-
-LOCALHOST = "127.0.0.1"
-HOST = LOCALHOST
-PORT = 21250
-CONN_QUEUE_SIZE = 5
-PACKET_SIZE = 2048
+# Create a socket object
 
 
-# === Function Definitions =========================================================================
+# Define the server address and port
 
-def open_socket (target_ip: str = "127.0.0.1", listen_port: int = 9999):
-    # Create a socket object
-    new_socket = socket.socket()
-    # Bind the socket to the server address
-    new_socket.bind ((target_ip, listen_port))
-    # Listen for incoming connections (max 5 clients in the queue)
-    new_socket.listen (CONN_QUEUE_SIZE)
-    print ("Server is listening on", target_ip)
-    return new_socket
 
-def get_connection (listen_socket: socket.socket) -> None:
-    unused_conn_info, addr_port = listen_socket.accept()
-    print (addr_port[0], "connected")
+# Bind the socket to the server address
+# Replace the following line with code to bind the socket
 
-def create_header (text: str) -> str:
-    if (text == "") or (text == "exit") or (text is None):
-        return "m_size 0"
-    else:
-        return ("m_size " + str (len (text)))
+# Listen for incoming connections (max 5 clients in the queue)
+# Replace the following line with code to listen for connections
 
-def encode_text (text: str):
-    return (text.encode ("utf-8"))
+print("Server is listening on", server_address)
 
-def decode_text (text) -> str:
-    return (text.decode ("utf-8"))
+while True:
+    # Wait for a client to connect
+    # Replace the following line with code to accept a client connection
+    
+    # Print a message to indicate the client connection
+    # Replace the following line with appropriate logging
 
-def get_message_size (header: str) -> int:
-    metadata = header.split (' ')
-    for i in enumerate (metadata):
-        if (i[0] == "m_size"):
-            return int (metadata[i[0] + 1])
-
-def receive_message (source: socket.socket) -> str:
-    try:
-        header = decode_text (source.recv (PACKET_SIZE))
-    except Exception:
-        return None
-    message_size = get_message_size (header)
-    receive_rounds = ceil (message_size / PACKET_SIZE)
-    try:
-        message = ""
-        for r in range (0, receive_rounds):
-            message += decode_text (source.recv (PACKET_SIZE))
-    except Exception:
-        return None
-    return message
-
-def echo_message (message: str, echo_destination: socket.socket) -> bool:
-    header = create_header (message)
-    try:
-        echo_destination.sendall (encode_text (header))
-        echo_destination.sendall (encode_text (message))
-    except Exception:
-        return False
-
-def echo_server (c_conn: socket.socket) -> None:
     # Handle client data
     while True:
-        try:
-            # Receive data from the client
-            message = receive_message (c_conn)
-            # Process and respond to the client's data
-            # Send the response back to the client
-            if (message is not None):
-                echo_message (message, c_conn)
-            else:
-                break
-        except Exception:
-            break
+        # Receive data from the client
+        # Replace the following line with code to receive data
+        # ensure you can receive long messages
+        
+        
+        # Process and respond to the client's data
+        # Replace the following line with your data processing logic
+        
+        # Send the response back to the client
+        # Replace the following line with code to send back the same message
 
-
-# === Main =========================================================================================
-
-if __name__ == "__main__":
-    c_connection = open_socket (HOST, PORT)
-    while True:
-        get_connection (c_connection)
-        echo_server (c_connection)
+    # Close the client socket
+    # Replace the following line with code to close the client socket
