e38c25a28ebb7c05a0d9943d81ab93008f1854a6
Drew Wheeler <wheelercs@protonmail.com>
2023-09-25 10:29:53 -0500

    Receiving and Parsing Header on Server
    Server-side header parsing is just about done, with payload being the next thing.

--- Project 02 - Network Packets/server.py
+++ Project 02 - Network Packets/server.py
@@ -19,16 +19,21 @@ ST_STR = 3
 def unpack_packet(conn: socket.socket, header_format: str) -> (str, str):
     # Receive first portion of packet
     client_packet = conn.recv (CONN_TRANS_SIZE)
+    print (client_packet)
     # Unpack packet and store header info
-    print (client_packet[:MIN_HEADER_SIZE])
     ver, h_len, s_type, p_len = struct.unpack (header_format, client_packet[:5])
-    packet_header_str: str = "Version: " + str (ver) + "\nHeader Len: " + str (h_len) + "\nService Type: " + str (s_type) + "\nPayload Length: " + str (p_len)
+    packet_header_str: str = ("Version: " + str (int.from_bytes (ver, "big")) +
+                              "\nHeader Len: " + str (int.from_bytes (h_len, "big")) +
+                              "\nService Type: " + str (int.from_bytes (s_type, "big")) +
+                              "\nPayload Length: " + str (p_len))
+    h_len = int.from_bytes (h_len, "big")
     # TODO: Adjust how to read info from connection based on payload service type
     # Analyze header for how much of payload remains and receive it
-    payload_str: str = ""
+    payload_str = client_packet[(h_len):].decode ("utf-8")
     receive_rounds = ceil ((p_len - (CONN_TRANS_SIZE - MIN_HEADER_SIZE)) / CONN_TRANS_SIZE)
-    for i in enumerate (receive_rounds):
-        pass
+    for count in range (0, receive_rounds):
+        temp_payload = conn.recv (CONN_TRANS_SIZE)
+        payload_str = payload_str + temp_payload
     # return the string - this will be the payload
     return (packet_header_str, payload_str)
 
@@ -38,7 +43,7 @@ if __name__ == '__main__':
     port = 12345
 
     # Fixed length header -> Version (1 byte), Header Length (1 byte), Service Type (1 byte), Payload Length (2 bytes)
-    header_format = "!BBBh"
+    header_format = "!ccch"
 
     print ("[Packet Server Started]")
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -49,9 +54,11 @@ if __name__ == '__main__':
             print(f"Connected by: {addr}")
             while True:
                 try:
-                    packet_header, payload_string = unpack_packet(conn, header_format)
-                    print (payload_string)
+                    packet_header, payload_string = unpack_packet (conn, header_format)
+                    print (packet_header)
+                    print ("Payload Received:\n" + (payload_string))
                 except Exception as e:
+                    print (e)
                     print("Connection closed or an error occurred")
                     break
 
