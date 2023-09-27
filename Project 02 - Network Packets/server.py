60de801726c034e91141154b4b0409652a7d1113
Drew Wheeler <wheelercs@protonmail.com>
2023-09-27 08:10:55 -0500

    Redoing Unpacking on Server
    Was having issues with the packet unpacking on server-side, so redoing code. Also making it s.t. it handles all data types that can be sent to it.

--- Project 02 - Network Packets/server.py
+++ Project 02 - Network Packets/server.py
@@ -16,28 +16,48 @@ ST_FLOAT = 2
 ST_STR = 3
 
 
-def unpack_packet(conn: socket.socket, header_format: str) -> (str, str):
+def unpack_packet(conn: socket.socket, header_format: str):
     # Receive first portion of packet
     client_packet = conn.recv (CONN_TRANS_SIZE)
     # Unpack packet and store header info
-    ver, h_len, s_type, p_len = struct.unpack (header_format, client_packet[:5])
-    packet_header_str: str = ("Version: " + str (int.from_bytes (ver, "big")) +
-                              "\nHeader Len: " + str (int.from_bytes (h_len, "big")) +
-                              "\nService Type: " + str (int.from_bytes (s_type, "big")) +
-                              "\nPayload Length: " + str (p_len))
+    ver, h_len, s_type, p_len = struct.unpack (header_format, client_packet[:MIN_HEADER_SIZE])
+    # Convert bin data to integers
+    ver = int.from_bytes (ver, "big")
     h_len = int.from_bytes (h_len, "big")
-    # TODO: Adjust how to read info from connection based on payload service type
-    # Analyze header for how much of payload remains and receive it
-    payload_str = client_packet[(h_len):].decode ("utf-8")
-    receive_rounds = ceil ((p_len - (CONN_TRANS_SIZE - MIN_HEADER_SIZE)) / CONN_TRANS_SIZE)
-    for count in range (0, receive_rounds):
-        temp_payload = conn.recv (CONN_TRANS_SIZE)
-        payload_str = payload_str + temp_payload
+    s_type = int.from_bytes (s_type, "big")
+    # Print header info
+    packet_header_str: str = ("Version: " + str (ver) +
+                              "\nHeader Len: " + str (h_len) +
+                              "\nService Type: " + str (s_type) +
+                              "\nPayload Length: " + str (p_len))
+    # Get part of payload that was received w/ header
+    raw_payload = client_packet[(h_len):]
+    payload = None
+
+    # Figure out how much data is extracted due to service type
+    if (s_type == ST_INT):
+        payload = int.from_bytes (raw_payload, "big")
+    elif (s_type == ST_FLOAT):
+        payload = struct.unpack ("!f", raw_payload)
+    elif (s_type == ST_STR):
+        payload = raw_payload.decode ("utf-8")
+
+    print (payload)
+    exit(22)
+
+    payload = ""
+
+
+
     # return the string - this will be the payload
-    return (packet_header_str, payload_str)
+    return (packet_header_str, payload)
 
 
 if __name__ == '__main__':
+    if (CONN_TRANS_SIZE < MIN_HEADER_SIZE):
+        print ("Size of packets received must be larger than minimum header size.")
+        exit (1)
+
     host = 'localhost'
     port = 12345
 
@@ -57,13 +77,15 @@ if __name__ == '__main__':
                     if (packet_header is not None):
                         print (packet_header)
                         print ("Payload Received:\n" + (payload_string))
-                except ConnectionError as ce:
+                except ConnectionError or ConnectionResetError as ce:
                     print (ce)
                     print ("Client disconnected")
+                    break
                 except Exception as e:
                     print (e)
                     print("Connection closed or an error occurred")
                     break
+
 
              #TODO: create header
 
